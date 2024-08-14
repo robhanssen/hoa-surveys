0afc523cebd49d108fe05a2426f7f989d209e846
Rob Hanssen <rob.hanssen@outlook.com>
2024-08-14 14:13:41 -0400

    initial results 8/14

---
+++ budget2025/budget2025.r
@@ -0,0 +1,101 @@
+library(tidyverse)
+library(patchwork)
+library(tidytext)
+library(wordcloud2)
+data(stop_words)
+
+theme_set(
+    theme_light() +
+        theme(
+            # axis.title = element_blank(),
+            panel.grid.minor = element_blank(),
+            legend.position = "none",
+            plot.title.position = "plot",
+            plot.caption.position = "plot",
+            plot.caption = element_text(hjust = 0)
+        )
+)
+
+datafiles <- list.files(path = "./budget2025/source", pattern = "*.csv$", full.names = TRUE)
+
+lvls <- c(
+    "$100 lower than FY2024",
+    "$50 lower than FY2024",
+    "Keep it the same as FY2024",
+    "$50 higher than FY2024",
+    "$100 higher than FY2024"
+)
+
+fee_question <-
+    map_df(datafiles[4], read_csv) %>%
+    mutate(
+        Answer = factor(Answer, levels = lvls, ordered = TRUE)
+    ) %>%
+    arrange(Answer) %>%
+    mutate( # correcting for my own test answer
+        Count = case_when(
+            Answer == lvls[5] ~ Count - 1,
+            TRUE ~ Count
+        ),
+        diff = c(-100, -50, 0, 50, 100)
+    )
+
+mean_fee_change <- weighted.mean(fee_question$diff, fee_question$Count)
+
+label_height <- max(fee_question$Count)
+
+fee_question %>%
+    ggplot(
+        aes(x = diff, y = Count)
+    ) +
+    geom_point(size = 3, shape = 1) +
+    geom_segment(
+        aes(x = diff, xend = diff, y = Count, yend = 0),
+        linewidth = 2, alpha = .5, color = "darkgreen"
+    ) +
+    annotate("text",
+        x = mean_fee_change + 5, y = label_height, hjust = 0,
+        label = glue::glue("Average:\n{scales::dollar(mean_fee_change)}")
+    ) +
+    geom_vline(xintercept = mean_fee_change, linewidth = 3, alpha = .25, color = "tan") +
+    labs(
+        x = "Desired fee change (in US$)",
+        Y = ""
+    ) +
+    scale_y_continuous(
+        breaks = seq(0, 400, 5)
+    )
+
+open_questions <-
+    map_df(datafiles, read_csv, .id = "file") %>%
+    filter(!str_detect(Answer, "testing"))
+
+
+words1_3 <-
+    open_questions %>%
+    filter(file %in% c(1:3)) %>%
+    select(Answer) %>%
+    unnest_tokens(word, Answer) %>%
+    count(word, sort = TRUE) %>%
+    anti_join(stop_words, by = "word")
+
+set.seed(12354) # for reproducibility
+
+wordcloud2(
+    data = words1_3, size = 1.3
+)
+
+
+words5_6 <-
+    open_questions %>%
+    filter(file %in% c(5:6)) %>%
+    select(Answer) %>%
+    unnest_tokens(word, Answer) %>%
+    count(word, sort = TRUE) %>%
+    anti_join(stop_words, by = "word")
+
+set.seed(12354) # for reproducibility
+
+wordcloud2(
+    data = words5_6, size = 1.3
+)
